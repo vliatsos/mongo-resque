4105a0036075dde37cc6f01fbeb246d84f4fdfe4
Chris Wanstrath <chris@ozmm.org>
2011-05-16 13:28:14 -0700

    ensure we're using bundler's test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift dir + '/../lib'
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default, :test)
+require 'resque' # because bundler looked for resque-igo
+
 $TESTING = true
 require 'test/unit'
-require 'rubygems'
-require 'resque'
 
 begin
   require 'leftright'
